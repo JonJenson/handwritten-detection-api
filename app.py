9d619ae45e5ea13b5cbe0074d793684cf44e1001
JonJenson <jonjenson2004@gmail.com>
2024-07-21 19:34:22 +0530

    init :)

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
-from flask import Flask, render_template, request, redirect, flash
-from pytorch_lightning import Trainer
+from flask import Flask, request, jsonify
 from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 from PIL import Image
-import os
 import torch
-from werkzeug.utils import secure_filename
+from flask_cors import CORS
+import io
 
 app = Flask(__name__)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-
-UPLOAD_FOLDER = "uploads"
-ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+CORS(app)
 
 # Load OCR model and processor
 model = VisionEncoderDecoderModel.from_pretrained(
@@ -19,11 +14,11 @@ model = VisionEncoderDecoderModel.from_pretrained(
 ).to("cuda" if torch.cuda.is_available() else "cpu")
 processor = TrOCRProcessor.from_pretrained("microsoft/trocr-large-handwritten")
 
+# Allowed file extensions
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
 
-# Function to check if file extension is allowed
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
-
 
 # OCR function
 def ocr(image):
@@ -34,39 +29,30 @@ def ocr(image):
     generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
     return generated_text
 
-
-# Homepage
-@app.route("/")
-def home():
-    return render_template("index.html")
-
+#test-endpoint
+@app.route("/api" , methods=["GET"])
+def get_method():
+    return jsonify({"msg" : "working ..."})
 
 # OCR endpoint
-@app.route("/upload", methods=["POST"])
+@app.route("/api/ocr", methods=["POST"])
 def upload_file():
-    if request.method == "POST":
-        # Check if the post request has the file part
-        if "file" not in request.files:
-            flash("No file part")
-            return redirect(request.url)
-        file = request.files["file"]
-        # If the user does not select a file, the browser submits an empty file without a filename
-        if file.filename == "":
-            flash("No selected file")
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            # Save the uploaded image
-            filename = secure_filename(file.filename)
-            file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
-            file.save(file_path)
-            # Perform OCR
-            image = Image.open(file_path).convert("RGB")
-            text = ocr(image)
-            return render_template("index.html", text=text)
-        else:
-            flash("Invalid file format. Allowed formats are: png, jpg, jpeg, gif")
-            return redirect(request.url)
+    app.logger.info(f"Request Data: {request.data}")
+    if "file" not in request.files:
+        return jsonify({"error": "No file part"}), 400
 
+    file = request.files["file"]
+
+    if file.filename == "":
+        return jsonify({"error": "No selected file"}), 400
+
+    if file and allowed_file(file.filename):
+        # Perform OCR
+        image = Image.open(io.BytesIO(file.read())).convert("RGB")
+        text = ocr(image)
+        return jsonify({"text": text})
+    else:
+        return jsonify({"error": "Invalid file format. Allowed formats are: png, jpg, jpeg, gif"}), 400
 
 if __name__ == "__main__":
     app.run(debug=True)
